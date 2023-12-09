32c446f72d3099caa44dbd310b7a2373ec4ca067
Pranaykumar30 <141347187+Pranaykumar30@users.noreply.github.com>
2023-12-09 15:58:09 +0530

    7) Check Prime.py

---
+++ Step 1 - Learn the basics/Step 1.4 - Know Basic Maths/7) Check Prime.py
@@ -0,0 +1,25 @@
+#https://www.codingninjas.com/studio/problems/check-prime_624934?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
+from math import *
+from collections import *
+from sys import *
+from os import *
+
+## Read input as specified in the question.
+## Print output as specified in the question.
+def is_prime(n):
+    if n == 2 or n == 3:
+        return True
+    if n ==1 or n % 2 == 0 or n % 3 ==0:
+        return False
+     
+    i = 5
+    while i *i <= n:
+       if n % i ==0 or n % (i + 2) ==0:
+          return False
+       i += 6
+    return True
+n = int(input())
+if is_prime(n):
+    print("YES")
+else:
+    print("NO")
